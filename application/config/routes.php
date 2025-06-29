021fe9410fac57a126211dd1809b27d698ee115f
Etom Usang <laiveetom@gmail.com>
2025-06-29 23:51:55 +0100

    Update routes.php default controller
    Changed blogcontroller to BlogController in the routes.php folder.

--- application/config/routes.php
+++ application/config/routes.php
@@ -49,7 +49,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | Examples:	my-controller/index	-> my_controller/index
 |		my-controller/my-method	-> my_controller/my_method
 */
-$route['default_controller'] = 'blogcontroller';
+$route['default_controller'] = 'BlogController';
 $route['post/add'] = 'BlogController/add_post';
 $route['404_override'] = '';
 $route['translate_uri_dashes'] = FALSE;
